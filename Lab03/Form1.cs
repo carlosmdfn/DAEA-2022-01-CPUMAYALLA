6a7c7fe236271349424a8f7577649c67735a8c1a
carlosmdfn <carlos.pumayalla@tecsup.edu.pe>
2022-04-04 22:15:53 -0500

    lab03

---
+++ Lab03/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAutenticacion.Checked)
+            {
+                txtUsuario.Enabled = false;
+                txtPassword.Enabled = false;
+            }
+            else
+            {
+                txtUsuario.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    conn.Close();
+                    MessageBox.Show("Conexión cerrada satisfactoriamente");
+                }
+                else
+                {
+                    MessageBox.Show("La conexión ya está cerrada");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al cerrar la conexión: \n" + ex.ToString());
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            String servidor = txtServidor.Text;
+            String bd = txtBaseDatos.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server=" + servidor + ";Database=" + bd + ";";
+
+           
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else
+                str += "User Id=" + user + ";Password=" + pwd + ";";
+
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conectado satisfactoriamente");
+                btnDesconectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar el servidor: \n" + ex.ToString());
+            }
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                    MessageBox.Show("Estado del servidor: " + conn.State +
+                        "\nVersión del servidor: " + conn.ServerVersion +
+                        "\nBase de datos: " + conn.Database);
+                else
+                    MessageBox.Show("Estado del servidor: " + conn.State);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del servidor: \n" +
+                    ex.ToString());
+            }
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            frmUsuarios usuarios = new frmUsuarios(conn);
+            usuarios.Show();
+        }
+    }
+}
---
+++ Lab03/frmUsuarios.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class frmUsuarios : Form
+    {
+        SqlConnection conn;
+        public frmUsuarios(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                String sql = "SELECT * FROM tbl_usuario";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("La conexión esta cerrada");
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                String data = txtNombre.Text;
+                String sql = String.Format("SELECT * FROM tbl_usuario WHERE usuario_nombre='{0}'",data);
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("La conexión esta cerrada");
+            }
+        }
+    }
+}
