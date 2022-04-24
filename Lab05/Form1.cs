ae7aacfedec14ae80d58e8f70fff9de969e2bbfd
carlosmdfn <carlos.pumayalla@tecsup.edu.pe>
2022-04-24 02:27:19 -0500

    Lab05

---
+++ Lab05/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab05
+{
+    public partial class ManPersonas : Form
+    {
+        SqlConnection con;
+
+        public ManPersonas()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+#pragma warning disable CS1009 // Secuencia de escape no reconocida
+            String str = "Server=DESKTOP-I9SU5QR\\LOCAL;Integrated Security=true;Database=School;";
+#pragma warning restore CS1009 // Secuencia de escape no reconocida
+            con = new SqlConnection(str);
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sql = "SELECT * from Person";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dgvListado.DataSource = dt;
+            dgvListado.Refresh();
+            con.Close();        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            if (txtCodigo.Text == null)
+            {
+                String sp = "BuscarPersonaNombre";
+                SqlCommand cmd = new SqlCommand(sp, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstName", txtNombre.Text);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+
+                con.Close();
+            }
+            else
+            {
+                String sp = "BuscarPersonaCodigo";
+                SqlCommand cmd = new SqlCommand(sp, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@PersonID", txtCodigo.Text);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+
+                con.Close();
+            }
+            
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "InsertPerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", txtNombre.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtApellido.Text);
+            cmd.Parameters.AddWithValue("@HireDate", dtContrato.Text);
+            cmd.Parameters.AddWithValue("@EnrollmentDate", dtInscripcion.Text);
+
+            int codigo = Convert.ToInt32(cmd.ExecuteScalar());
+            MessageBox.Show("Se ha registrado nueva persona con el código: " + codigo);
+            con.Close();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "UpdatePerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PersonID", txtCodigo.Text);
+            cmd.Parameters.AddWithValue("@FirstName", txtNombre.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtApellido.Text);
+            cmd.Parameters.AddWithValue("@HireDate", dtContrato.Text);
+            cmd.Parameters.AddWithValue("@EnrollmentDate", dtInscripcion.Text);
+
+            int resultado = cmd.ExecuteNonQuery();
+            if (resultado > 0)
+            {
+                MessageBox.Show("Se ha modificado el registro correctamente");
+            }
+            con.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "DeletePerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PersonID", txtCodigo.Text);
+
+            int resultado = cmd.ExecuteNonQuery();
+
+            if (resultado > 0)
+            {
+                MessageBox.Show("Se ha eliminado el registro correctamente");
+            }
+            con.Close();
+        }
+
+        private void dgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvListado_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvListado.SelectedRows.Count > 0)
+            {
+                txtCodigo.Text = dgvListado.SelectedRows[0].Cells[0].Value.ToString();
+                txtNombre.Text = dgvListado.SelectedRows[0].Cells[1].Value.ToString();
+                txtApellido.Text = dgvListado.SelectedRows[0].Cells[2].Value.ToString();
+                dtContrato.Text = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                dtInscripcion.Text = dgvListado.SelectedRows[0].Cells[4].Value.ToString();
+            }
+        }
+    }
+}
--- Lab05/Properties/AssemblyInfo.cs
+++ Lab05/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Lab02-01")]
+[assembly: AssemblyTitle("Lab05")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab02-01")]
+[assembly: AssemblyProduct("Lab05")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("095b9dd3-d77f-4f08-bfda-e9d959d1d53b")]
+[assembly: Guid("9028fe89-ef91-42e5-a3c5-a21c45cfa175")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
