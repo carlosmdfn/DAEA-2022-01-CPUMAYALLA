bed5e5dc7be0883e0d54ce2238b3e37ff7115f80
carlosmdfn <carlos.pumayalla@tecsup.edu.pe>
2022-06-30 11:46:09 -0500

    lab16

---
+++ lab16/Models/PersonContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using persons.Models;
+
+namespace persons.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
